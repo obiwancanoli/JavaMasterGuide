875a062bb68eff08436f81c0e39ecab0bbd1cd1d
Daniel Murillo <dmurillo21291@gmail.com>
2024-08-15 16:35:05 -0700

    Add more Pied Piper to SimpleGame for test.

--- ContinuePractice/src/SimpleGame.java
+++ ContinuePractice/src/SimpleGame.java
@@ -4,6 +4,17 @@ public class SimpleGame {
 	public static void main(String[] args) {
 
 		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		System.out.println("I am Pied Piper");
+		
 	}
 
 }
