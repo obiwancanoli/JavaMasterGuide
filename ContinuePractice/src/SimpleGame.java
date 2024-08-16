8088026e09d8973a9af8a9bfca6dbb2a6664b1eb
Daniel Murillo <daniel.murillo@mediamonks.com>
2024-08-16 15:11:15 -0700

    Add Cooper message to SimepleGame

--- ContinuePractice/src/SimpleGame.java
+++ ContinuePractice/src/SimpleGame.java
@@ -3,17 +3,7 @@ public class SimpleGame {
 
 	public static void main(String[] args) {
 
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
-		System.out.println("I am Pied Piper");
+		System.out.println("Cooper is the best dog in the world!");
 		
 	}
 
